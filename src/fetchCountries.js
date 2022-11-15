24e73ed03d93673379e765f5ece7c3fb878ce276
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-11-15 15:58:26 +0200

    add code

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+      // Notify.failure('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
+}
+// fetchCountries();
+// export default fetchCountries();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,103 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
 
-const DEBOUNCE_DELAY = 300;
+//
+// import { templateFunction } from './templates/country-list.hbs';
+// console.log(Handlebars.VERSION);
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryCard: document.querySelector('.country-info'),
+
+  DEBOUNCE_DELAY: 300,
+};
+
+// refs.countryList.addEventListener('click', onCountryListClick);
+refs.input.addEventListener(
+  'input',
+  debounce(onInputFunc, refs.DEBOUNCE_DELAY)
+);
+// function onCountryListClick(e) {
+//   const chooseCountry = e.target;
+//   return chooseCountry;
+// }
+//
+
+function onInputFunc(e) {
+  e.preventDefault();
+
+  let nameValue = refs.input.value.trim();
+
+  fetchCountries(nameValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        refs.countryList.innerHTML = '';
+
+        return;
+      } else if (countries.length >= 2 && countries.length <= 10) {
+        renderUserList(countries);
+      } else {
+        renderCountryCard(countries);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      Notify.failure('Oops, there is no country with that name');
+      refs.countryCard.innerHTML = '';
+      refs.countryList.innerHTML = '';
+    });
+}
+
+// console.log('name', nameValue);
+
+function renderUserList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <p><b><img src = "${country.flags.svg}" alt="Flag" width = "30px"/></b> ${country.name.official}</p>    
+            </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
+  refs.countryCard.innerHTML = '';
+  refs.countryList.addEventListener('click', onCountryListClick);
+  function onCountryListClick(e) {
+    if (e.target) {
+      renderCountryCard(e.target.childNodes[1].data);
+    }
+    console.dir(e.target.childNodes[1].data);
+  }
+}
+
+function renderCountryCard(countries) {
+  const markup = countries
+    .map(country => {
+      return `<p><b><img src = "${
+        country.flags.svg
+      }" alt="Flag" width = "30px"/></b> ${country.name.official}</p>
+              <p><b>Capital</b>: ${country.capital}</p>
+              <p><b>population</b>: ${country.population}</p>
+              <p><b>languages</b>: ${Object.values(country.languages)}</p>
+
+            </li>`;
+    })
+    .join('');
+  refs.countryCard.innerHTML = markup;
+  refs.countryList.innerHTML = '';
+}
+
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
+// Notify.failure('Qui timide rogat docet negare');
+// Notify.info('Cogito ergo sum');
+// Notify.warning('Memento te hominem esse');
+// Notify.success('Sol lucet omnibus');
